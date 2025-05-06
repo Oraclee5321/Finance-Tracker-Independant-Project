363197c47ba5a61e3e7702138066b889991ff1b8
Alex Cooper <98158236+Oraclee5321@users.noreply.github.com>
2025-05-06 18:55:05 +0100

    Start

---
+++ Finance Tracker New/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Finance_Tracker_New;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Finance Tracker New/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Finance_Tracker_New;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
