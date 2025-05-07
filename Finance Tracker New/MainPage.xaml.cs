38c6a1dfd5540822784d8ba3b2693c198503e38b
Alex Cooper <cooperalex5321@gmail.com>
2025-05-07 13:11:18 +0100

    Base Need to fix DevPage

--- Finance Tracker New/MainPage.xaml.cs
+++ Finance Tracker New/MainPage.xaml.cs
@@ -1,23 +1,50 @@
-﻿namespace Finance_Tracker_New;
+﻿using System.Windows.Input;
+
+namespace Finance_Tracker_New;
+using SkiaSharp;
+using SkiaSharp.Views.Maui;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
+    
+    public string CurrentSpending { get; set; } = "£0";
+    public string TotalBudget { get; set; } = "£600";
+    
     public MainPage()
     {
         InitializeComponent();
+        BindingContext = this;
     }
+    
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs e)
     {
-        count++;
+        var canvas = e.Surface.Canvas;
+        canvas.Clear(SKColors.White);
+        
+        var data = new List<float> {25, 35, 40};
+        var colors = new List<SKColor> {SKColors.Red, SKColors.Green, SKColors.Blue};
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+        float startAngle = 0;
+        var center = new SKPoint(e.Info.Width / 2, e.Info.Height / 2);
+        float radius = Math.Min(e.Info.Width, e.Info.Height) / 2;
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        for (int i = 0; i < data.Count; i++)
+        {
+            using var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = colors[i],
+            };
+            
+            float sweepAngle = 360 * (data[i] / data.Sum());
+            var path = new SKPath();
+            path.MoveTo(center);
+            path.ArcTo(new SKRect(center.X - radius, center.Y -  radius, center.X + radius, center.Y + radius),startAngle, sweepAngle, false);
+            path.Close();
+            canvas.DrawPath(path, paint);
+            startAngle += sweepAngle;
+            
+        }
     }
 }
--- Finance Tracker New/MauiProgram.cs
+++ Finance Tracker New/MauiProgram.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+
 
 namespace Finance_Tracker_New;
 
 public static class MauiProgram
 {
+
+    
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseSkiaSharp()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
